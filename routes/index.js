d8a3a5eec3e116ad4b6d77efa35cb29e8e13315f
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-29 17:18:26 -0400

    routes/index usercities

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const cityRouter = require('./city')
+const cityRouter = require('./users')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -17,6 +18,7 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.use('/city', cityRouter)
+router.use('/users', userCities)
 
 
 module.exports = router;
