a317960a20d650e232a162aeaaa850411cd68ec5
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-29 16:16:17 -0400

    cityRouter

---
+++ routes/city.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+//traerme el metodo con desestructuracion
+const { create } = require('../controllers/cityController')//aca me traigo una propiedad/metodo del objeto/controlador
+//traerme el metodo con el objeto completo
+// const cityController = require('../controllers/cityController')// aca me traigo todo el objeto/controlador
+// const createController = cityController.create
+// const readController = cityController.read
+// const destroyController = cityController.destroy
+// const updateController = cityController.update
+
+
+/* GET users listing. */
+
+// router.metodo('la ruta',controlador)
+
+router.post('/', create);
+
+//localhost4000/events/
+
+module.exports = router;
