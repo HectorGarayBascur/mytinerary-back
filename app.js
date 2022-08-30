54a9492d711f5f4beac8a0e87a08c76b2743a3a1
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-30 14:57:09 -0400

    require cityRouter tu app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var cityRouter = require('./routes/cities');
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/cities', cityRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
