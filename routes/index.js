d25062ffdf000a83803736c52e91321d0d71a79c
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-30 09:26:08 -0400

    change title

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const cityRouter = require('./city')
-const cityRouter = require('./users')
+const cityRouter = require('./cities')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json([]);
+  res.render('index', { title: 'HOLAAAAAAAAAAAAAAAAA' });
 });
 
-router.get('/:id', function (req, res, next) {
-  if (req.params.id == 123) {
-    return res.status(404).json()
-  }
-  res.json({
-    id: req.params.id
-  })
-});
+// router.get('/:id', function (req, res, next) {
+// //   if (req.params.id == 123) {
+// //     return res.status(404).json()
+// //   }
+// //   res.json({
+// //     id: req.params.id
+// //   })
+// // });
 
-router.use('/city', cityRouter)
-router.use('/users', userCities)
+router.use('/cities', cityRouter)
+router.use('/comments', cityRouter)
 
 
 module.exports = router;
