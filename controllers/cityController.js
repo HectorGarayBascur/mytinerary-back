26ccac39e727977a2ebfa201e381ed9fce8e3995
Hector Garay <hectorgaraybascur@gmail.com>
2022-08-30 09:24:18 -0400

    fixed line success

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -7,7 +7,7 @@ const cityController = {
             await new City(req.body).save()// req.body tiene que tener todas las variables antes descritas
             res.status(201).json({
                 message: 'City created',
-                succes: true
+                success: true
             })
         } catch (error) {
             res.status(400).json({
