9cb5c0a284ca4cb37f3e32c5302b9944a7c8f005
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-09-07 11:45:52 -0300

    rutas para usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { create } = require('../controllers/userController')
+const { all,create } = require('../controllers/userController')
 
 
 /* GET users listing. */
 
+
+router.get("/",all)
 router.post('/auth', create);
 
 module.exports = router;
