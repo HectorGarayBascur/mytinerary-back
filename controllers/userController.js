b9ea35b72043d50926a4fb3afba00c99ad6389ab
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-09-07 11:43:16 -0300

    controladores para users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,32 @@ const { query } = require('express')
 const User = require('../models/User')
 
 const userController = {
+    all : async (req,res) => {
+        let users
+        let query={}
+
+        if(req.query.users){
+    query.users=req.query.users
+            }
+
+        try {
+            users= await User.find(query)
+        
+            res.status(200).json({
+                message:"you get all users",
+                response: users,
+                succes:true
+            })
+        }
+
+        catch(error){
+            res.status(500).json
+        }
+
+    },
+
+
+
     create: async (req, res) => {
         try {
             await new User(req.body).save()
