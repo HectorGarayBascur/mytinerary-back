88a1f0188f52597af681fe3547ae250128714ff2
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-09-07 11:45:30 -0300

    rutas para itinerarios

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { create, update, destroy } = require('../controllers/itineraryController')
+const {itineraries, itinerariesByCity, create, update, destroy } = require('../controllers/itineraryController')
 
 
 /* GET users listing. */
-
+router.get("/",itineraries)
+router.get("/:id",itinerariesByCity)
 router.post('/', create);
 router.patch('/:id', update);
 router.delete('/:id', destroy);
