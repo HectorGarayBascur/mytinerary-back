9d60a54a64c8fa53c674a4058c494b93d87565cd
juanmoyano123 <moyanojjeronimo@gmail.com>
2022-09-07 11:42:52 -0300

    controlador para itinerarios

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -1,10 +1,70 @@
 const { query } = require('express')
+const { populate } = require('../models/Itinerary')
 const Itinerary = require('../models/Itinerary')
 
 const itineraryController = {
+    
+    itineraries: async (req,res) => {
+        let itineraries
+        let query={}
+
+       //Itinerarios by esto es para USER
+
+        if(req.query.user){
+    query.user=req.query.user
+            }
+
+                //Itinerarios by esto es para CITIES
+
+            if(req.query.city){
+                query.city=req.query.city
+                        }
+
+        try {
+            itineraries= await Itinerary.find(query)
+            .populate("user",{name:1})
+            .populate("city",{city:1})
+            res.status(200).json({
+                message:"itineraries by city",
+                response: itineraries,
+                succes:true
+            })
+        }
+
+        catch(error){
+            res.status(500).json
+        }
+
+    },
+
+itinerariesByCity: async (req,res)=> {
+
+let query= {}
+if(req.query.cities) {
+
+    query.cities=req.query.cities
+}
+if (query.itineraries) {
+    
+    query.cities=req.query.cities
+}
+
+    try {
+        let itineraries= await Itinerary.find(query)
+        .populate("city")
+    
+    }
+    catch(error){
+        res.status(500).json
+    }
+
+},
+
     create: async (req, res) => {
+
         try {
             await new Itinerary(req.body).save()
+            .populate("itinerary")
             res.status(201).json({
                 message: 'Itinerary created',
                 success: true
