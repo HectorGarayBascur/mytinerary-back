ca1e2a6f9634688e33a08887ef670caff3fbd39c
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-12 13:16:20 -0300

    Adding signin route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const { all, signUp } = require('../controllers/userController')
+const { all, signUp, verifyMail, signIn } = require('../controllers/userController')
 
 
 /* GET users listing. */
 
 
 router.get("/", all)
+router.get("/verify/:code", verifyMail)
 router.post('/auth', signUp);
+router.post("/signin", signIn)
 
 module.exports = router;
