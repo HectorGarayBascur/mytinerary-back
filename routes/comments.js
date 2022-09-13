aae5e67dcea06759164bbea4994f9f089c38a87c
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-13 10:28:55 -0300

    Adding new route to comment

--- routes/comments.js
+++ routes/comments.js
@@ -4,12 +4,13 @@ var router = express.Router()
 const {
     create,
     all,
+    read,
 
-  
 } = require('../controllers/commentController')
 
 
 router.post('/', create)
 router.get('/', all)
+router.get('/:itineraryid', read)
 
 module.exports = router
