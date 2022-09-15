45aa7d3b18dcdc4d49774bb509d387d55e8de3d5
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-15 12:13:48 -0300

    tests de registro de usuarios

---
+++ test/usersTest.js
@@ -0,0 +1,53 @@
+const request = require("supertest");
+const app = require("../app");
+const { assert } = require("chai");
+
+describe("POST /users/auth", function () {
+  it("Must respond with id", function (done) {
+    request(app)
+      .post("/users/auth")
+      .send({
+        name: "Carlos",
+        photo:
+          "http://www.istockphoto.com/resources/images/PhotoFTLP/1040315976.jpg",
+        mail: "moyanojjeronimo+4568@gmail.com",
+        password: "hola.",
+        role: "user",
+        from: "from",
+        lastName: "moyano",
+        country: "argentina",
+      })
+      .then((response) => {
+        assert.isString(response.body.id);
+        return done();
+      });
+  });
+
+  it("must respond with 201", function (done) {
+    request(app)
+      .post("/users/auth")
+      .send({
+        name: "Carlos",
+        photo:
+          "http://www.istockphoto.com/resources/images/PhotoFTLP/1040315976.jpg",
+        mail: "moyanojjeronimo+12377554@gmail.com",
+        password: "hola.",
+        role: "user",
+        from: "from",
+        lastName: "moyano",
+        country: "argentina",
+      })
+      .expect(201, done);
+  });
+
+  it("Must respond with 400 status", function (done) {
+    request(app)
+      .post("/users/auth")
+      .send({})
+      .expect(400)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
